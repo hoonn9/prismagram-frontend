8ab8c94dc999f490c55fc9df58d89fa279f3fe73
HXXNS <39939869+HXXNS@users.noreply.github.com>
2020-02-14 02:58:28 +0900

    #9.2

---
+++ src/Components/FollowButton/FollowButtonPresenter.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Button from "../Button";
+export default ({ isFollowing, onClick }) => {
+  return (
+    <Button text={isFollowing ? "Unfollow" : "Follow"} onClick={onClick} />
+  );
+};
--- src/Components/Post/PostContainer.js
+++ src/Components/Post/PostContainer.js
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import useInput from "../../Hooks/useInput";
 import PostPresenter from "./PostPresenter";
+import { useMutation } from "react-apollo-hooks";
+import { TOGGLE_LIKE, ADD_COMMENT } from "./PostQueries";
+import { toast } from "react-toastify";
 
 const PostContainer = ({
   id,
@@ -10,9 +13,92 @@ const PostContainer = ({
   likeCount,
   isLiked,
   comments,
-  createdAt
+  createdAt,
+  caption,
+  location
 }) => {
-  return <PostPresenter />;
+  const [isLikedS, setIsLiked] = useState(isLiked);
+  const [likeCountS, setLikeCount] = useState(likeCount);
+  const [currentItem, setCurrentItem] = useState(0);
+  const [selfComments, setSelfComments] = useState([]);
+  const comment = useInput("");
+  const [toggleLikeMutation] = useMutation(TOGGLE_LIKE, {
+    variables: { postId: id }
+  });
+  const [addCommentMutation] = useMutation(ADD_COMMENT, {
+    variables: { postId: id, text: comment.value }
+  });
+
+  const slide = () => {
+    const totalFiles = files.length;
+    if (totalFiles) {
+      if (currentItem === totalFiles - 1) {
+        setTimeout(() => setCurrentItem(0), 3000);
+      } else {
+        setTimeout(() => setCurrentItem(currentItem + 1), 3000);
+      }
+    }
+  };
+
+  useEffect(() => {
+    slide();
+  }, [currentItem]);
+
+  const toggleLike = async () => {
+    await toggleLikeMutation();
+    if (isLikedS === true) {
+      setIsLiked(false);
+      setLikeCount(likeCountS - 1);
+    } else {
+      setIsLiked(true);
+      setLikeCount(likeCountS + 1);
+    }
+  };
+
+  const onKeyPress = async event => {
+    const { which } = event;
+    if (which === 13) {
+      event.preventDefault();
+      try {
+        const {
+          data: { addComment }
+        } = await addCommentMutation();
+        setSelfComments([...selfComments, addComment]);
+        comment.setValue("");
+      } catch {
+        toast.error("Cant send comment");
+      }
+      //fake component 방식
+      // setSelfComments([
+      //   ...selfComments,
+      //   {
+      //     id: Math.floor(Math.random() * 100),
+      //     text: comment.value,
+      //     user: { username: meQuery.me.username }
+      //   }
+      // ]);
+    }
+  };
+
+  return (
+    <PostPresenter
+      user={user}
+      files={files}
+      likeCount={likeCountS}
+      location={location}
+      caption={caption}
+      isLiked={isLikedS}
+      comments={comments}
+      createdAt={createdAt}
+      newComment={comment}
+      setIsLiked={setIsLiked}
+      setLikeCount={setLikeCount}
+      currentItem={currentItem}
+      toggleLike={toggleLike}
+      onKeyPress={onKeyPress}
+      selfComments={selfComments}
+    />
+  );
 };
 
 PostContainer.propTypes = {
@@ -40,7 +126,9 @@ PostContainer.propTypes = {
       }).isRequired
     })
   ).isRequired,
-  createdAt: PropTypes.string
+  caption: PropTypes.string.isRequired,
+  location: PropTypes.string,
+  createdAt: PropTypes.string.isRequired
 };
 
 export default PostContainer;
